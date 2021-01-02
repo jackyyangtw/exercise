5c67cc1b8f800ffe5f46786f128b7bd815daa06e
JackyYang <jaky2204@gmail.com>
2021-01-02 17:06:28 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <div class="jumbotron jumbotron-fluid jumbotron-bg d-flex align-items-end m-0">
+    <loading :active.sync="isLoading" ></loading>
+    <div class="toTop bg-light p-2 rounded-circle">
+      <a><i class="fas fa-arrow-up text-warning"></i></a>
+    </div>
+    <div class="jumbotron jumbotron-fluid jumbotron-bg d-flex align-items-end m-0 bg-warning" >
       <div class="container">
         <div class="p-1 bg-lighter">
           <h1 class="display-5 font-weight-bold text-center">台灣旅遊網</h1>
@@ -8,6 +12,22 @@
         </div>
       </div>
     </div>
+    <form class="form-inline my-3 my-lg-0 m-auto" id="searchBar">
+      <div class="input-group text-center p-3">
+        <label for="">請輸入縣市名稱:</label>
+        <input class="form-control ml-1" type="text" v-model="searchText"
+          placeholder="臺北市、臺東縣" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button"
+            @click="searchText = ''">
+            清除
+          </button>
+        </div>
+        <select name="region" id="" v-for="item in regionData" :key="item.region">
+          <option value="">{{ item.Region }}</option>
+        </select>
+      </div>
+    </form>
     <table class="table">
       <thead>
         <tr>
@@ -17,10 +37,10 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
+        <tr v-for="data in filterData " :key="data.Id" >
+          <th scope="row" >{{ data.Region }}</th>
+          <td>{{ data.Town }}</td>
+          <td>{{ data.Name }}</td>
         </tr>
       </tbody>
     </table>
@@ -28,27 +48,69 @@
 </template>
 
 <script>
+import $ from 'jquery'
 export default {
   name: 'App',
-  components: {
-  },
   data(){
     return {
-      data: []
+      datas: [],
+      searchText: '',
+      isLoading: false,
+    }
+  },
+  computed: {
+    filterData() {
+      const vm = this;
+      if (vm.searchText) {
+        return vm.datas.filter((item) => {
+          const apiRegion = item.Region.toLowerCase().includes(vm.searchText.toLowerCase());
+          return apiRegion;
+        });
+      }
+      return vm.datas;
+    },
+    regionData(){
+      const vm = this
+      return vm.datas.filter((item)=>{
+        return item.Region === vm.datas.Region
+      })
     }
   },
   created(){
-    // this.$http.get("api/open-api/swagger/docs/V1").then((response) => {
-    //   console.log(response.data)
-    // })
+    const vm = this
+    vm.isLoading = true
+    
     this.$http({
-          url: "/api",
-          method: "get",
-          crossdomain: true,
-        }).then((res) => {
-          // const resObject = JSON.parse(res)
-          console.log(res);
-        });
+      url: "/api/XMLReleaseALL_public/scenic_spot_C_f.json",
+      method: "get",
+      crossdomain: true,
+    }).then((res) => {
+      console.log(res.data.XML_Head);
+      let Info = res.data.XML_Head.Infos.Info
+      let newInfo = []
+      Info.filter(item=>{
+        if(item.Region !== null){
+          newInfo.push(item)
+        }
+      })
+      vm.datas = newInfo
+      vm.isLoading = false
+    });
+  },
+  mounted (){
+    $(document).ready(()=>{
+      $('.toTop').click((event) => {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: 0 }, 500);
+      });
+      $(window).scroll(function(){
+        if ($(window).scrollTop() >= 50) {
+          $('.toTop').fadeIn();
+        } else {
+          $('.toTop').fadeOut();
+        }
+      })
+    })
   }
 }
 </script>
@@ -62,5 +124,12 @@ export default {
   -moz-osx-font-smoothing: grayscale
   text-align: center
   color: #2c3e50
-
+.toTop
+  position: fixed
+  right: 10px
+  bottom: 30px
+  cursor: pointer
+  z-index: 100
+  i
+    font-size: 50px
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios' //主要ajax套件
 import VueAxios from 'vue-axios' //轉為vue套件
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css'
+
 Vue.use(VueAxios, axios)
 
 
 Vue.config.productionTip = false
+Vue.component('Loading', Loading);
 
 
 new Vue({
